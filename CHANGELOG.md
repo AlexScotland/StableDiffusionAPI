f40f0188893276be2b02510948630ebae6589812
Alex Scotland <alex@scotland.ca>
2024-08-01 02:51:47 -0400

    chore(changelog): Updates Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 1.1.0
+- Adds API endpoint saving manually added safetensors
+- Adds new AbstractImagePipeline, allowing compatibility for SDXL and SD1.5 basemodels.
+- Small Code Cleanup
+  
 # 1.0.0
 - Adds API endpoint for getting all models
   - This can now be used in `LoRA POST` request
--- models/code_models/abstract_image_pipeline.py
+++ models/code_models/abstract_image_pipeline.py
@@ -43,12 +43,12 @@ class AbstractImagePipeline(BaseImagePipeline):
     def create_image(
             self,
             prompt,
-            height=600,
-            width=600,
-            lora_choice: LoRA=None,
-            negative_prompt= "easynegative, human, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot,",
-            steps=50,
-            number_of_images=1,
+            height,
+            width,
+            lora_choice: LoRA,
+            negative_prompt,
+            steps,
+            number_of_images,
         ):
             if lora_choice:
                 self.pipeline.load_lora_weights(
