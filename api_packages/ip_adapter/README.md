5104d55a55f0c52f470f0e71331ca3f57130221a
Alex Scotland <alex@scotland.ca>
2024-11-01 01:56:49 -0400

    feat(ip): Adds ip package

---
+++ api_packages/ip_adapter/README.md
@@ -0,0 +1,21 @@
+# IP Adapters
+This package implements IP Adapter API endpoints.
+
+This is utilizing [Tencent AI Labs IP Adapter](https://github.com/tencent-ailab/IP-Adapter)
+
+# Installation
+to use this package, the following is necessary:
+ - [ ] Install the Requirements
+ - [ ] Install the Models
+
+# Requirements Installation
+1. pip install git+https://github.com/tencent-ailab/IP-Adapter.git
+or
+1. pip install -r .requirements.txt
+
+# Model Installation
+1. cd IP-Adapter
+2. git lfs install
+3. git clone https://huggingface.co/h94/IP-Adapter
+4. mv IP-Adapter/models models
+5. mv IP-Adapter/sdxl_models sdxl_models
---
+++ api_packages/ip_adapter/routers/router.py
@@ -0,0 +1,44 @@
+import io
+
+from fastapi import APIRouter, Response, UploadFile, Depends, File
+
+from settings import MODEL_DIRECTORY
+
+from models.code_models.image_to_image_request import Image2ImageRequest
+from models.code_models.abstract_image_to_image_pipeline import AbstractImageToImagePipeline
+
+from routers.v2 import find_lora_by_name, __clean_up_pipeline
+
+
+ROUTER = APIRouter(
+    prefix="/ip_adapter",
+    tags=["IP Adapters for Same character images"]
+)
+
+@ROUTER.post("/generate/")
+def generate_picture_from_picture(image: Image2ImageRequest = Depends(), uploaded_image: UploadFile=File(...)):
+    # TODO: Dreambooth instead of base_lora
+    contextual_lora = find_lora_by_name(
+        image.lora_choice,
+        image.pipeline
+        )
+    pipeline = AbstractImageToImagePipeline(
+        MODEL_DIRECTORY, 
+        image.pipeline)
+    image_store = io.BytesIO()
+    for generated_image in pipeline.generate_image(
+        image.prompt,
+        uploaded_image,
+        image.strength,
+        image.guidance,
+        height=image.height,
+        width=image.width,
+        negative_prompt=image.negative_prompt,
+        lora_choice=contextual_lora):
+        generated_image.save(image_store,"png")
+        break
+
+    # Cleanup our pipeline
+    __clean_up_pipeline(pipeline)
+
+    return Response(content=image_store.getvalue(), media_type="image/png")
